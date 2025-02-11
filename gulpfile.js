51adf4e35b3acb67dc67c5a6277ec95b5374ad35
showzyl <showzyl520@gmail.com>
2025-02-12 02:11:40 +0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -26,41 +26,38 @@ const paths = {
   assets: path.join(__dirname, '../src/assets'),
 }
 
-gulp.task('fonts', function() {
+function fonts() {
   return gulp.src([
       'node_modules/font-awesome/fonts/fontawesome-webfont.*'])
     .pipe(gulp.dest('dist/fonts/'))
     .pipe($.size())
-})
+}
 
-gulp.task('scripts', () => {
+function scripts() {
   return gulp.src([
       'node_modules/jquery/dist/jquery.min.js',
       'node_modules/velocity-animate/velocity.js',
       paths.scripts
     ])
     .pipe($.uglify())
-    .pipe($.concat({ path: 'scripts.js', stat: { mode: 0666} }))
+    .pipe($.concat({ path: 'scripts.js', stat: { mode: '0666'} }))
     .pipe(gulp.dest('dist/assets/'))
     .pipe($.size())
-})
+}
 
-gulp.task('styles', () => {
+function styles() {
   return gulp.src([
       'node_modules/font-awesome/css/font-awesome.min.css',
       paths.styles
     ])
     .pipe($.stylus({ use: nib(), compress: true, import: ['nib']}))
-    .pipe($.autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-    .pipe($.concat({ path: 'styles.css', stat: { mode: 0666} }))
+    .pipe($.autoprefixer())
+    .pipe($.concat({ path: 'styles.css', stat: { mode: '0666'} }))
     .pipe(gulp.dest('dist/assets/'))
     .pipe($.size())
-})
+}
 
-gulp.task('html', () => {
+function html() {
   const MarkdownType = new yaml.Type('tag:yaml.org,2002:md', {
     kind: 'scalar',
     construct: function (text) {
@@ -74,15 +71,22 @@ gulp.task('html', () => {
     .pipe($.htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist'))
     .pipe($.size())
-})
+}
 
-gulp.task('default', ['scripts', 'styles', 'fonts','html'], () => {
-  if (isProd) return
+function watch() {
   browserSync.init({
     server: "./dist"
   })
-  gulp.watch(paths.scripts, ['scripts'])
-  gulp.watch(paths.styles, ['styles'])
-  gulp.watch(['template/*.html', 'data.yaml'], ['html'])
+  gulp.watch(paths.scripts, scripts)
+  gulp.watch(paths.styles, styles)
+  gulp.watch(['template/*.html', 'data.yaml'], html)
   gulp.watch(["dist/*.html", "dist/assets/*.*"]).on('change', browserSync.reload)
-})
+}
+
+const build = gulp.parallel(scripts, styles, fonts, html)
+
+exports.fonts = fonts
+exports.scripts = scripts 
+exports.styles = styles
+exports.html = html
+exports.default = isProd ? build : gulp.series(build, watch)
--- public.js
+++ public.js
@@ -4,18 +4,26 @@ const path = 'dist'
 const repo = 'https://github.com/showzyl/resume.git'
 
 console.log(`Start public to your git repo[${repo}]\nPlease wait ...`.green)
-git(path)
-  .init()
-  .add('./*')
-  .commit("feat: build file")
-  .addRemote('origin', repo)
-  .push(['-f', 'origin', 'master'], () => {
-    console.log("Push to master success");
-  })
-  .checkoutLocalBranch('gh-pages', () => {
-    console.log('Checkout to branch gh-pages');
-  })
-  .push(['-f', 'origin', 'gh-pages'])
-  .checkout('master', () => {
-    console.log('Finish public, back to branch master.');
-  })
+
+async function deployToGit() {
+  try {
+    const gitClient = git(path)
+    await gitClient.init()
+    await gitClient.add('./*')
+    await gitClient.commit("feat: build file") 
+    await gitClient.addRemote('origin', repo)
+    await gitClient.push(['-f', 'origin', 'main'])
+    console.log("Push to main success")
+    
+    await gitClient.checkoutLocalBranch('gh-pages')
+    console.log('Checkout to branch gh-pages')
+    
+    await gitClient.push(['-f', 'origin', 'gh-pages'])
+    await gitClient.checkout('main')
+    console.log('Finish public, back to branch main')
+  } catch (err) {
+    console.error('Deploy failed:', err)
+  }
+}
+
+deployToGit()
